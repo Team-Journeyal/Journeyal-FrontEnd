97f6542a379ed345d4e378f3a8e9e60c1fb1309f
Corey <coreydblevins@gmail.com>
2022-12-10 22:13:15 -0500

    register

--- App.js
+++ App.js
@@ -1,17 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
+
 import { Button, StyleSheet, Text, View } from 'react-native';
 import { useState } from 'react';
 import Login from './components/Login.js'
+import Register from './components/Register.js'
 
 export default function App() {
   const [loggedIn, setLoggedIn] = useState(false)
 
   return (
     <View style={styles.container}>
+
       <Text>Hello World</Text>
-      <Button onPress={() => {setLoggedIn(true)}}>to login</Button>
-      {loggedIn && <Login />}
-      <StatusBar style="auto" />
+      <Button onPress={() => {setLoggedIn(!loggedIn)}} title="Login" />
+      {loggedIn ? <Login /> : <Register/>}
+    
     </View>
   );
 }
