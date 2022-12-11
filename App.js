3149d769402d3d11e00d1325e7876e9c5ffde2c9
Corey <coreydblevins@gmail.com>
2022-12-10 21:58:10 -0500

    state

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import Login from './components/Login.js'
 
 export default function App() {
+  const [loggedIn, setLoggedIn] = useState(false)
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello World</Text>
+      <Button onPress={() => {setLoggedIn(true)}}>to login</Button>
+      {loggedIn && <Login />}
       <StatusBar style="auto" />
     </View>
   );
