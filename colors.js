635d51b7e4c5344505a6554cf359ed49bf6e731b
Taylor <tbnelson01@gmail.com>
2022-12-12 10:40:34 -0500

    styled login and register

---
+++ colors.js
@@ -0,0 +1,7 @@
+export default {
+  background: "#95D5B2",
+  light: "#D8F3DC",
+  dark: "#081C15",
+  bright: "#52B788",
+  white: "#fff",
+};
--- components/LoginScreen.js
+++ components/LoginScreen.js
@@ -1,79 +1,96 @@
-import { View, Text, StyleSheet, TextInput, Pressable, Button } from 'react-native';
-import { useState } from 'react';
-import Register from './RegisterScreen.js';
-import Home from './HomeScreen.js';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Pressable,
+  Button,
+  SafeAreaView,
+  StatusBar,
+} from "react-native";
+import { useState } from "react";
+import Register from "./RegisterScreen.js";
+import Home from "./HomeScreen.js";
+import colors from "../colors.js";
 
 export default function Login({ loggedIn, setLoggedIn }) {
-    const [register, setRegister] = useState(false)
+  const [register, setRegister] = useState(false);
 
-    if (register) return <Register/>
-    
-    return (
-        <View style={styles.background}>
-            {loggedIn ? (<Home/>) :(
-                <>
-            <View style={styles.header}>
-                <Text style={styles.title}>Journeyal</Text>
-            </View>
-                    <View style={styles.login}>
-                        <TextInput placeholder="username" style={styles.inputs}></TextInput>
-                        <TextInput placeholder="password" style={styles.inputs}></TextInput>
-                        <Pressable style={styles.button} onPress={() => setLoggedIn(true)}>
-                            <Text>Log In</Text>
-                        </Pressable>
-                        <Button title='press it' onPress={() => setRegister(true)}/>
-                    </View>
-                    </>
-                )}
-        </View>
-    )
+  if (register) return <Register />;
+
+  return (
+    <View style={styles.background}>
+      {loggedIn ? (
+        <Home />
+      ) : (
+        <>
+          <StatusBar barStyle={"light-content"} />
+          <View style={styles.header}>
+            <Text style={styles.title}>Journeyal</Text>
+          </View>
+          <View style={styles.login}>
+            <TextInput placeholder="username" style={styles.inputs}></TextInput>
+            <TextInput
+              secureTextEntry={true}
+              placeholder="password"
+              style={styles.inputs}
+            ></TextInput>
+            <Pressable style={styles.button} onPress={() => setLoggedIn(true)}>
+              <Text>Log In</Text>
+            </Pressable>
+            <Button title="Register" onPress={() => setRegister(true)} />
+          </View>
+        </>
+      )}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    background: {
-        flex: 1,
-        backgroundColor: 'grey',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    button: {
-        backgroundColor: 'yellow',
-        borderWidth: 2,
-        borderColor: 'black',
-        borderRadius: 5,
-        height: 40,
-        width: 60,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    header: {
-        position: 'absolute',
-        top: 0,
-        width: '100%',
-        height: 200,
-        backgroundColor: 'black',
-        alignItems: 'center',
-        justifyContent: 'flex-end'
-    },
-    inputs: {
-        borderWidth: 2,
-        borderColor: 'black',
-        borderRadius: 5,
-        margin: 10,
-        width: 130,
-    },
-    login: {
-        height: 300,
-        width: 300,
-        borderWidth: 2,
-        borderColor: 'black',
-        borderRadius: 5,
-        backgroundColor: 'white',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    title: {
-        fontSize: 40,
-        color: 'white',
-    },
+  background: {
+    flex: 1,
+    backgroundColor: colors.background,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    backgroundColor: colors.bright,
+    borderWidth: 2,
+    borderColor: colors.dark,
+    borderRadius: 5,
+    height: 40,
+    width: 60,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  header: {
+    position: "absolute",
+    top: 0,
+    width: "100%",
+    height: "20%",
+    backgroundColor: colors.dark,
+    alignItems: "center",
+    justifyContent: "flex-end",
+  },
+  inputs: {
+    borderWidth: 2,
+    borderColor: colors.dark,
+    borderRadius: 5,
+    margin: 10,
+    width: 130,
+  },
+  login: {
+    height: 300,
+    width: 300,
+    borderWidth: 2,
+    borderColor: colors.dark,
+    borderRadius: 5,
+    backgroundColor: colors.white,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 40,
+    color: colors.white,
+  },
 });
