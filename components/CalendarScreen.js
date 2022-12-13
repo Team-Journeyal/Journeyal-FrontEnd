209fae7c4ed3bcf08126873d4836a471ab04fcf0
Taylor <tbnelson01@gmail.com>
2022-12-13 10:06:16 -0500

    marked dates

--- components/CalendarScreen.js
+++ components/CalendarScreen.js
@@ -1,12 +1,34 @@
-import { View, Text, StyleSheet, ScrollView, StatusBar } from "react-native";
+import { useState } from "react";
+import { View, StyleSheet, StatusBar } from "react-native";
 import { Calendar } from "react-native-calendars";
+import colors from "../colors";
 import CalendarScroll from "./CalendarScroll";
 
 export default function CalendarScreen() {
+  const [date, setDate] = useState("");
+
   return (
     <View style={styles.background}>
       <StatusBar barStyle={"light-content"} />
-      <Calendar />
+      <Calendar
+        style={{
+          backgroundColor: colors.light,
+        }}
+        theme={{
+          calendarBackground: colors.light,
+          selectedDayBackgroundColor: colors.bright,
+        }}
+        onDayPress={(day) => {
+          setDate(day.dateString);
+        }}
+        markedDates={{
+          [date]: {
+            selected: true,
+            selectedColor: colors.bright,
+          },
+        }}
+        initialDate={date}
+      />
       <CalendarScroll />
     </View>
   );
