3708d0bd72638a9601cf9f8d7ed10891ab60d486
Taylor <tbnelson01@gmail.com>
2022-12-13 22:32:30 -0500

    basic layout of a add entry

--- App.js
+++ App.js
@@ -1,16 +1,16 @@
+import { Button } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LoginScreen from "./components/LoginScreen.js";
 import RegisterScreen from "./components/RegisterScreen.js";
 import HomeScreen from "./components/HomeScreen.js";
 import CalendarScreen from "./components/CalendarScreen.js";
+import NewEntryScreen from "./components/NewEntryScreen.js";
 import colors from "./colors.js";
-import data from "./sample.json";
 
 const Stack = createNativeStackNavigator();
 
-
-export default function App() {
+export default function App({ navigation }) {
   return (
     <NavigationContainer>
       <Stack.Navigator>
@@ -36,6 +36,21 @@ export default function App() {
         <Stack.Screen
           name="Calendar"
           component={CalendarScreen}
+          options={({ navigation }) => ({
+            headerStyle: { backgroundColor: colors.dark },
+            headerTitleStyle: { color: colors.white },
+            headerRight: () => (
+              <Button
+                title="+"
+                color={"white"}
+                onPress={() => navigation.navigate("Add")}
+              />
+            ),
+          })}
+        />
+        <Stack.Screen
+          name="Add"
+          component={NewEntryScreen}
           options={{
             headerStyle: { backgroundColor: colors.dark },
             headerTitleStyle: { color: colors.white },
