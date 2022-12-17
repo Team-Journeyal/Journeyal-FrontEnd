96260d8845c985f15bc770aa3235f7ec2e443c63
Corey <coreydblevins@gmail.com>
2022-12-16 22:24:43 -0500

    added more endpoints to requests.js

--- components/Requests.js
+++ components/Requests.js
@@ -5,18 +5,16 @@ export const requestLogin = (username, password) => {
 
   const response = axios.post(url, {
     username: username,
-    password: password,
-  });
-  return response;
+    password: password })
+  return response
 };
 
-export const requestCalendars = (token) => {
-  const url = "https://journeyal-webservice.onrender.com/calendar/";
+export const requestLogout = (token) => {
+  const url = "https://journeyal-webservice.onrender.com/auth/token/logout/"
 
-  const response = axios.get(url, {
-    headers: { Authorization: `Token ${token}` },
-  });
-  return response;
+  const response = axios.post(url, {
+    headers: { Authorization: `Token ${token}` }})
+  return response
 };
 
 export const requestNewUser = (username, password) => {
@@ -24,9 +22,16 @@ export const requestNewUser = (username, password) => {
 
   const response = axios.post(url, {
     username: username,
-    password: password,
-  });
-  return response;
+    password: password })
+  return response
+};
+
+export const requestCalendars = (token) => {
+  const url = "https://journeyal-webservice.onrender.com/calendar/";
+
+  const response = axios.get(url, {
+    headers: { Authorization: `Token ${token}` }})
+  return response
 };
 
 export const requestNewCalendar = (token, name) => {
@@ -34,6 +39,43 @@ export const requestNewCalendar = (token, name) => {
 
   const response = axios.post(url, { 
     name: name},
-    {headers: { Authorization: `Token ${token}`}})
+    {headers: { Authorization: `Token ${token}` }})
   return response
-}
+};
+
+export const requestEditCalendar = (token, name, id) => {
+  const url = `https://journeyal-webservice.onrender.com/calendar/${id}/`
+
+  const response = axios.patch(url, {
+    name: name}, 
+    {headers: { Authorization: `Token ${token}` }})
+  return response
+};
+
+export const requestDeleteCalendar = (token, id) => {
+  const url = `https://journeyal-webservice.onrender.com/calendar/${id}/`
+
+  const response = axios.delete(url, {
+    headers: { Authorization: `Token ${token}` }})
+  return response
+};
+
+export const requestAllEntries = (token) => {
+  const url = "https://journeyal-webservice.onrender.com/journal/"
+
+  const response = axios.get(url, {
+    headers: { Authorization: `Token ${token}` }})
+  return response
+};
+
+export const requestAddEntry = (token, date, calendar, entry, event) => {
+  const url = "https://journeyal-webservice.onrender.com/journal/"
+
+  const response = axios.post(url, {
+    date: date,
+    calendar: calendar,
+    entry: entry,
+    event: event},
+    {headers: { Authorization: `Token ${token}` }})
+  return response
+};
