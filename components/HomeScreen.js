081e049e79dc0b4b7cb6253e22a8f5e89c273fc6
Taylor <tbnelson01@gmail.com>
2022-12-19 16:10:58 -0500

    calendar states updating as intended on selection

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,68 +1,84 @@
-import { View, ScrollView, Text, Image, StyleSheet, Pressable, TextInput } from "react-native";
+import {
+  View,
+  ScrollView,
+  Text,
+  Image,
+  StyleSheet,
+  Pressable,
+  TextInput,
+} from "react-native";
 import { useState, useEffect } from "react";
 import colors from "../colors";
-import { requestCalendars, requestNewCalendar, requestCalendarsEntries } from "./Requests";
+import {
+  requestCalendars,
+  requestNewCalendar,
+  requestCalendarsEntries,
+} from "./Requests";
 
 export default function HomeScreen({ navigation, route }) {
   const [calendars, setCalendars] = useState([]);
-  const [calendarName, setCalendarName] = useState("")
-  const [refresh, setRefresh] = useState(false)
-  const [calendarId, setCalendarId] = useState("")
+  const [calendarName, setCalendarName] = useState("");
+  const [refresh, setRefresh] = useState(false);
+  const [calendarId, setCalendarId] = useState("");
 
   useEffect(() => {
     requestCalendars(route.params.token).then((response) =>
       setCalendars(response.data)
-    )
+    );
   }, [route.params.token, refresh]);
 
   const handleSubmit = () => {
-      requestNewCalendar(route.params.token, calendarName)
-      setRefresh(!refresh)
-      setCalendarName("")
-  }
+    requestNewCalendar(route.params.token, calendarName);
+    setRefresh(!refresh);
+    setCalendarName("");
+  };
 
   const handleCalendarEntries = (clndr) => {
-    setCalendarId(clndr.id)
-    console.log(`in ${calendarId}`)
+    setCalendarId(clndr.id);
+    console.log(`in ${clndr.id}`);
 
     // .then(requestCalendarsEntries(route.params.token, calendarId))
-    
-    navigation.navigate("Calendar", {calendarId: calendarId})
-  }
-  console.log(`outside ${calendarId}`)
+
+    navigation.navigate("Calendar", { calendarId: clndr.id });
+  };
+  console.log(`outside ${calendarId}`);
 
   return (
     <View style={styles.background}>
-    <ScrollView style={{width: "100%"}}>
-    <View style={styles.container}>
-      <Text>Hello, {route.params.username}</Text>
-      {calendars.map((clndr, idx) => (
-        <Pressable
-          key={idx}
-          style={styles.button}
-          onPress={async () => {handleCalendarEntries(clndr)}}
-        >
-          <Image style= {styles.image} source={{uri: "https://picsum.photos/200/300"}}/>
-          <Text style={styles.text}>{clndr.name}</Text>
-        </Pressable>
-      ))}
-      <View style={styles.submitBox}>
-        <Pressable 
-        style={styles.button} 
-        onPress={handleSubmit}>
-            <Text>Add calendar</Text>
-        </Pressable>
-        <TextInput
-            autoCorrect={false}
-            autoCapitalize="none"
-            value={calendarName}
-            placeholder="New Calendar"
-            onChangeText={setCalendarName}
-            style={styles.inputs}/>
+      <ScrollView style={{ width: "100%" }}>
+        <View style={styles.container}>
+          <Text>Hello, {route.params.username}</Text>
+          {calendars.map((clndr, idx) => (
+            <Pressable
+              key={idx}
+              style={styles.button}
+              onPress={() => {
+                handleCalendarEntries(clndr);
+              }}
+            >
+              <Image
+                style={styles.image}
+                source={{ uri: "https://picsum.photos/200/300" }}
+              />
+              <Text style={styles.text}>{clndr.name}</Text>
+            </Pressable>
+          ))}
+          <View style={styles.submitBox}>
+            <Pressable style={styles.button} onPress={handleSubmit}>
+              <Text>Add calendar</Text>
+            </Pressable>
+            <TextInput
+              autoCorrect={false}
+              autoCapitalize="none"
+              value={calendarName}
+              placeholder="New Calendar"
+              onChangeText={setCalendarName}
+              style={styles.inputs}
+            />
+          </View>
+        </View>
+      </ScrollView>
     </View>
-      </View>
-    </ScrollView>
-      </View>
   );
 }
 
@@ -103,7 +119,6 @@ const styles = StyleSheet.create({
     alignItems: "center",
     borderWidth: 2,
     borderColor: colors.dark,
-
   },
   text: {
     color: colors.white,
